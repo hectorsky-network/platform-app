5ec0f69b8648fee050b8e2df6bdfe36e99ace17d
Administrator <hector.wilde@bk.ru>
2017-12-01 18:38:51 +0100

    launcher fix.

--- app/Http/Controllers/LauncherController.php
+++ app/Http/Controllers/LauncherController.php
@@ -11,25 +11,25 @@ use Illuminate\Support\Facades\Storage;
 class LauncherController extends Controller
 {
     protected function launcherupdate(){
-        $build = 24;
+        $build = 3;
         $response = array(
             'build' => $build,
             'url' => array(
-                'exe'=>"https://developer.hectorwilde.com/build/browse/HP-LAUN-".$build."/artifact/shared/Windows/launcher-4.0-".$build.".exe",
-                'jar'=>"https://developer.hectorwilde.com/build/browse/HP-LAUN-".$build."/artifact/shared/Linux/launcher-4.0-".$build.".jar",
-                'osx'=>"https://developer.hectorwilde.com/build/browse/HP-LAUN-".$build."/artifact/shared/Mac/launcher-4.0-".$build."-osx.app.zip"),
+                'exe'=>"https://cdn.hectorwilde.com/projects/hectorplatform/launcher/builds/'.$build'/launcher-4.0-".$build.".exe",
+                'jar'=>"https://cdn.hectorwilde.com/projects/hectorplatform/launcher/builds/'.$build'/launcher-4.0-".$build.".jar",
+                'osx'=>"https://cdn.hectorwilde.com/projects/hectorplatform/launcher/builds/'.$build'/launcher-4.0-".$build."-osx.app.zip"),
             'resources' => array(
                 array(
                     'filename' => "OpenSans+Cyberbit.ttf",
-                    'url' => "https://cdn.hectorwilde.com/mcrepo/launcher/resources/OpenSans+Cyberbit.ttf",
+                    'url' => "https://cdn.hectorwilde.com/projects/hectorplatform/launcher/OpenSans+Cyberbit.ttf",
                     'md5' => "94a9d4cbcb9ceaa25000fa303fa81acc"),
                 array(
                     'filename' => "Raleway+FireflySung.ttf",
-                    'url' => "https://cdn.hectorwilde.com/mcrepo/launcher/resources/Raleway+FireflySung.ttf",
+                    'url' => "https://cdn.hectorwilde.com/projects/hectorplatform/launcher/Raleway+FireflySung.ttf",
                     'md5' => "b62087eb4b11746987c800c0512890ef"),
                 array(
                     'filename' => "aether-dep.jar",
-                    'url' => "https://cdn.hectorwilde.com/mcrepo/launcher/resources/aether-dep.jar",
+                    'url' => "https://cdn.hectorwilde.com/projects/hectorplatform/launcher/aether-dep.jar",
                     'md5' => "EBF34784444ABCB976F77C9E23692057")));
         return response()->json($response);
     }
