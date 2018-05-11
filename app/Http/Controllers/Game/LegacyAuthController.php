d41ac17df6e01f09b56eaedda7fc1426998603ef
Daniel Burzmiński <hector.wilde@bk.ru>
2018-05-12 00:03:55 +0200

    lel

--- app/Http/Controllers/Game/LegacyAuthController.php
+++ app/Http/Controllers/Game/LegacyAuthController.php
@@ -11,7 +11,7 @@ class LegacyAuthController extends Controller
 {
     public function joinServer(){
 		if($_REQUEST['user'] === User::where('name',$_REQUEST['user'])->value('name')){
-		AuthServer::where('id',User::where('name',$_REQUEST['user']->value('id')))->update(['session' => $_REQUEST['serverId'], 'server' => $_REQUEST['sessionId']]);
+		AuthServer::where('id',User::where('name',$_REQUEST['user'])->value('id'))->update(['session' => $_REQUEST['serverId'], 'server' => $_REQUEST['sessionId']]);
 		echo 'OK';
 		}else{
 		echo 'Bad Login';
