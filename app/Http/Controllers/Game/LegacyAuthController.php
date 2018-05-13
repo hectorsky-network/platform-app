ec2a11aee9ff7dc7aa21c607f80b79b4d1b4aa7b
Daniel Burzmiński <hector.wilde@bk.ru>
2018-05-13 12:30:12 +0200

    same for legacy endpoint.

--- app/Http/Controllers/Game/LegacyAuthController.php
+++ app/Http/Controllers/Game/LegacyAuthController.php
@@ -9,20 +9,28 @@ use App\AuthServer;
 class LegacyAuthController extends Controller
 {
     public function joinServer(){
-		$idses = substr($_REQUEST['sessionId'], 6,32);
-		if($idses === AuthServer::where('access_token',$idses)->value('access_token')){
-		AuthServer::where('access_token',$idses)->update(['session' => $idses, 'server' => $_REQUEST['serverId']]);
-		echo 'OK';
-		}else{
-		echo 'Bad Login';
-		}
+        if(isset($_REQUEST['sessionId']) || isset($_REQUEST['serverId'])) {
+            $idses = substr($_REQUEST['sessionId'], 6, 32);
+            if ($idses === AuthServer::where('access_token', $idses)->value('access_token')) {
+                AuthServer::where('access_token', $idses)->update(['session' => $idses, 'server' => $_REQUEST['serverId']]);
+                echo 'OK';
+            } else {
+                echo 'Bad Login';
+            }
+        }else{
+            abort(403);
+        }
 	}
 	
 	public function checkServer(){
-		if($_REQUEST['serverId'] === AuthServer::where('server',$_REQUEST['serverId'])->value('server')){
-			echo "YES";
-		}else{
-			echo "NO";
-		}
+        if(isset($_REQUEST['serverId'])) {
+            if ($_REQUEST['serverId'] === AuthServer::where('server', $_REQUEST['serverId'])->value('server')) {
+                echo "YES";
+            } else {
+                echo "NO";
+            }
+        }else{
+            abort(403);
+        }
 	}
 }
