09156eac3f90cd84dcf9b83687d2bd06007dad39
Daniel Burzmiński <hector.wilde@bk.ru>
2018-05-13 10:51:16 +0200

    fixed old links.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,13 +42,13 @@
                             <a class="nav-link" href="/"><i class="fa fa-newspaper-o" aria-hidden="true"></i> Aktualności <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item {{{ (Request::is('launcher') ? 'active' : '') }}}">
-                            <a class="nav-link" href="https://wiki.hectorwilde.com/Hector_Platform#Pobieranie_launchera"><i class="fa fa-download" aria-hidden="true"></i> Launcher <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="https://hectorwilde.com/files/category/8-launcher/"><i class="fa fa-download" aria-hidden="true"></i> Launcher <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item {{{ (Request::is('modpacks') ? 'active' : '') }}}">
                             <a class="nav-link" href="{{ route('modpacks') }}"><i class="fa fa-inbox" aria-hidden="true"></i> Paczki Modyfikacji <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://forums.hectorwilde.com/" target="_blank"><i class="fa fa-wrench" aria-hidden="true"></i> Forum wsparcia <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="https://hectorwilde.com/forums/" target="_blank"><i class="fa fa-wrench" aria-hidden="true"></i> Forum wsparcia <span class="sr-only">(current)</span></a>
                         </li>
                     </ul>
 
