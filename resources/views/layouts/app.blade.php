358029a4394192838d1d97e60ee6d38d5aaf54f1
Daniel Burzmiński <hector.wilde@bk.ru>
2018-08-18 21:08:17 +0200

    minework

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>mcHub - Najlepsza zamknięta platforma modyfikacji</title>
+    <title>mineWORK - Najlepsza zamknięta platforma modyfikacji</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
